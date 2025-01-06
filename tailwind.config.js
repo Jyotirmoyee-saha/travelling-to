c872f6c5cd97684a278d7af165a8e3d4be76bbc4
Jyotirmoyee-saha <sahajyoti2000@gmail.com>
2025-01-06 17:28:58 +0530

    Created Nav Bar and Hero Section base

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navBar: "#132240",
+      }
+    },
+  },
+  plugins: [],
+}
+
