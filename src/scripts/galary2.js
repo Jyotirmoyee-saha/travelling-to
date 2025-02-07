20fb9f367cea9d42597041ec343e8e1f99b4616b
Jyotirmoyee-saha <sahajyoti2000@gmail.com>
2025-02-07 17:27:08 +0530

    new  commit for responsiveness and some js functions

---
+++ src/scripts/galary2.js
@@ -0,0 +1,18 @@
+[
+    {
+        "image":"../assets/g5.jpg"
+        
+    },
+    {
+        "image":"../assets/g6.jpg"
+    },
+        
+    {
+        "image":"../assets/g7.jpg"
+    },
+       
+    {
+        "image":"../assets/g8.jpg"
+       
+    }
+]
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navBar: "#132240",
+      },
+      screens: {
+        '3xs': '240px',
+        '2xs': '320px',
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
+
