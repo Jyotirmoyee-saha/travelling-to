638daa2d817d6ffdfdabac7eeec29fb07f7b478d
Jyotirmoyee-saha <sahajyoti2000@gmail.com>
2025-02-07 17:54:22 +0530

    test

---
+++ src/dist/scripts/galary.js
@@ -0,0 +1,19 @@
+[
+    {
+        "image":"../assets/g1.jpg"
+        
+    },
+    {
+        "image":"../assets/g2.jpg"
+    },
+        
+    {
+        "image":"../assets/g3.jpg"
+    },
+       
+    {
+        "image":"../assets/g4.jpg"
+       
+    }
+]
+
